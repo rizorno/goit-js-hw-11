2ac5d1065824dc1511e8c2c2178c135c139d82f3
rizorno <114782234+rizorno@users.noreply.github.com>
2023-01-11 00:30:41 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -5,12 +5,36 @@ import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const { searchForm, gallery, loadMoreBtn, endCollectionText } = {
+const { searchForm, submitBtn, gallery, loadMoreBtn, endCollectionText } = {
   searchForm: document.querySelector('.search-form'),
+  submitBtn: document.querySelector('.search-form__button'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
   endCollectionText: document.querySelector('.end-collection-text'),
 };
+
+// Create 'label' for Pagination
+searchForm.insertAdjacentHTML(
+  'beforeend',
+  "<label class='pagination'>Pagination by:</label >"
+);
+const labelPagination = document.querySelector('.pagination');
+
+// Create button 'Load more'
+labelPagination.insertAdjacentHTML(
+  'beforeend',
+  "<div class='box-pagination'><button type='button' class='btn-load-more'>Load more</button></div>"
+);
+const paginationBtn = document.querySelector('.btn-load-more');
+paginationBtn.disabled = true;
+
+// Create button 'Scroll'
+paginationBtn.insertAdjacentHTML(
+  'afterend',
+  "<button type='button' class='btn-scroll'>Scroll</button>"
+);
+const paginationScroll = document.querySelector('.btn-scroll');
+paginationScroll.classList.toggle('js-bg');
 
 // Function for creating Card template
 function cardTemplate({
@@ -82,9 +106,8 @@ async function onSubmitSearchForm(e) {
   const response = await fetchImages(searchQuery, currentPage);
   currentHits = response.hits.length;
 
-  if (response.totalHits > 40) {
-    loadMoreBtn.classList.remove('is-hidden'); // for solution #1
-    //  document.addEventListener('scroll', onScrollDocument); // for solution #2
+  if (response.totalHits > 40 && paginationBtn.disabled) {
+    loadMoreBtn.classList.remove('is-hidden');
   } else {
     loadMoreBtn.classList.add('is-hidden');
   }
@@ -119,6 +142,43 @@ async function onSubmitSearchForm(e) {
   }
 }
 
+// Add Event Listener on button 'Load more'
+paginationBtn.addEventListener('click', onPaginBtn);
+
+async function onPaginBtn(e) {
+  paginationBtn.disabled = true;
+  paginationScroll.disabled = false;
+
+  paginationBtn.classList.toggle('js-bg');
+  paginationScroll.classList.toggle('js-bg');
+
+  document.removeEventListener('scroll', onScrollDocument);
+
+  const response = await fetchImages(searchQuery, currentPage);
+  if (searchQuery === '') {
+    loadMoreBtn.classList.add('is-hidden');
+  } else if (currentHits !== response.totalHits) {
+    loadMoreBtn.classList.remove('is-hidden');
+  } else if (currentHits === response.totalHits && currentHits > 40) {
+    loadMoreBtn.classList.add('is-hidden');
+    endCollectionText.classList.remove('is-hidden');
+  } else {
+    endCollectionText.classList.add('is-hidden');
+  }
+}
+
+// Add Event Listener on button 'Scroll'
+paginationScroll.addEventListener('click', onPaginScroll);
+
+async function onPaginScroll(e) {
+  paginationBtn.disabled = false;
+  paginationScroll.disabled = true;
+  paginationBtn.classList.toggle('js-bg');
+  paginationScroll.classList.toggle('js-bg');
+  loadMoreBtn.classList.add('is-hidden');
+  document.addEventListener('scroll', onScrollDocument);
+}
+
 //* Solution #1 : Button 'Load more'
 
 // Add Event Listener on button 'Load more'
@@ -139,30 +199,30 @@ async function onClickLoadMoreBtn() {
 
 //* Solution #2 : Infinite scroll
 
-// // Create Event for scroll
-// let isActiveQuery = false;
-// let onScrollDocument = e => {
-//   const { clientHeight, scrollHeight, scrollTop } = e.target.documentElement;
-//   const currentScrollTop = scrollHeight - scrollTop - clientHeight;
-//   if (currentScrollTop < 600 && !isActiveQuery) {
-//     isActiveQuery = true;
-//     loadNextPage();
-//   }
-// };
+// Create Event for scroll
+let isActiveQuery = false;
+let onScrollDocument = e => {
+  const { clientHeight, scrollHeight, scrollTop } = e.target.documentElement;
+  const currentScrollTop = scrollHeight - scrollTop - clientHeight;
+  if (currentScrollTop < 600 && !isActiveQuery) {
+    isActiveQuery = true;
+    loadNextPage();
+  }
+};
 
-// // Add Event Listener on scroll
+// Add Event Listener on scroll // if we use only solution #2
 // document.addEventListener('scroll', onScrollDocument);
 
-// async function loadNextPage() {
-//   currentPage += 1;
-//   const response = await fetchImages(searchQuery, currentPage);
-//   renderCardImage(response.hits);
-//   isActiveQuery = false;
-//   lightbox.refresh();
-//   currentHits += response.hits.length;
+async function loadNextPage() {
+  currentPage += 1;
+  const response = await fetchImages(searchQuery, currentPage);
+  renderCardImage(response.hits);
+  isActiveQuery = false;
+  lightbox.refresh();
+  currentHits += response.hits.length;
 
-//   if (currentHits === response.totalHits) {
-//     document.removeEventListener('scroll', onScrollDocument);
-//     endCollectionText.classList.remove('is-hidden');
-//   }
-// }
+  if (currentHits === response.totalHits) {
+    document.removeEventListener('scroll', onScrollDocument);
+    endCollectionText.classList.remove('is-hidden');
+  }
+}
